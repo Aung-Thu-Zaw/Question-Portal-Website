1b317c631a88a70ce7a6882542ab3500238bc5c6
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2023-01-25 12:01:16 +0630

    vue3 + laravel9 setup with vite

---
+++ resources/js/layouts/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-red-700 text-5xl">This is Vue Page</h1>
+  <router-view />
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style>
+</style>
---
+++ resources/js/views/Welcom.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Hello World</h1>
+  <p class="text-emerald-600">This is from vue</p>
+</template>
