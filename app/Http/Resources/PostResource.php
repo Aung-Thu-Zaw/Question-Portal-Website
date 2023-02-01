824aa7c5049700cfa4cfd2a3c2bcbc844a412125
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2023-02-01 13:03:40 +0630

    create api for question posts

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+             "id"=>$this->id,
+             "question"=>$this->question,
+             "detail"=>$this->detail,
+             "view"=>$this->view,
+             "created_date"=>$this->created_at->toDateString(),
+             "created_time"=>$this->created_at->diffForHumans(),
+             "updated_date"=>$this->updated_at->toDateString(),
+         ];
+    }
+}
